cb84b32be32b60eca361616c8a826326bb521e8d
Igor Matias <146603895+tadziomatias@users.noreply.github.com>
2024-08-31 18:54:40 -0300

    Update mapa_especies.py
    Adição de arquivos kmz com layout do aeroporto SBJP

--- mapa_especies.py
+++ mapa_especies.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import zipfile
+import os
+import folium
+from folium.plugins import MarkerCluster
+import xml.etree.ElementTree as ET
 
 # Função para converter coordenadas de graus, minutos e segundos para graus decimais
 def dms_to_dd(coord):
@@ -35,40 +40,29 @@ print(df.head())
 df['Lat'] = df['Lat'].apply(dms_to_dd)
 df['Long'] = df['Long'].apply(dms_to_dd)
 
-import zipfile
-import os
-
+# Função para extrair KMZ e converter para KML
 def kmz_to_kml(kmz_file, output_dir):
     with zipfile.ZipFile(kmz_file, 'r') as z:
         z.extractall(output_dir)
-    # Procura o arquivo KML extraído
     for file in os.listdir(output_dir):
         if file.endswith('.kml'):
             return os.path.join(output_dir, file)
     return None
 
 # Diretório temporário para salvar o arquivo KML extraído
-output_dir = '/Users/matias/Documents/Programação/mapa_especies'
+output_dir = '/Users/matias/Documents/Programação/mapa_especies/kmz'
 
-# Caminho do arquivo KMZ
-kmz_file = '/Users/matias/Documents/Programação/mapa_especies/GE_SBJP_Quadrantes.kmz'
-
-# Converte KMZ para KML
-kml_file = kmz_to_kml(kmz_file, output_dir)
-
-import folium
-from folium.plugins import MarkerCluster
-
-# Criar um mapa centrado em uma localização específica
-mapa = folium.Map(location=[-7.146954, -34.952295], zoom_start=16)
-
-folium.TileLayer('openstreetmap').add_to(mapa)
-
-folium.LayerControl().add_to(mapa)
+# Caminhos dos arquivos KMZ
+kmz_files = [
+    '/Users/matias/Documents/Programação/mapa_especies/kmz/quadrantes.kmz',
+    '/Users/matias/Documents/Programação/mapa_especies/kmz/operacional.kmz',
+    '/Users/matias/Documents/Programação/mapa_especies/kmz/patrimonial.kmz',
+    '/Users/matias/Documents/Programação/mapa_especies/kmz/patio.kmz',
+    '/Users/matias/Documents/Programação/mapa_especies/kmz/ppd.kmz'
+]
 
 # Função para adicionar o conteúdo do arquivo KML ao mapa
-def add_kml_to_map(kml_file, mapa):
-    import xml.etree.ElementTree as ET
+def add_kml_to_map(kml_file, mapa, line_color='red'):
     tree = ET.parse(kml_file)
     root = tree.getroot()
     namespace = {'kml': 'http://www.opengis.net/kml/2.2'}
@@ -80,39 +74,44 @@ def add_kml_to_map(kml_file, mapa):
         if len(coords) == 1:  # ponto
             folium.Marker(location=[coords[0][1], coords[0][0]], popup=f'{name}: {description}').add_to(mapa)
         else:  # linha ou polígono
-            folium.PolyLine(locations=[(coord[1], coord[0]) for coord in coords], popup=f'{name}: {description}').add_to(mapa)
+            folium.PolyLine(locations=[(coord[1], coord[0]) for coord in coords], popup=f'{name}: {description}', color=line_color, weight=1, opacity=1.0).add_to(mapa)
 
-# Adiciona o conteúdo do KML ao mapa
-if kml_file:
-    add_kml_to_map(kml_file, mapa)
+# Criar um mapa centrado em uma localização específica
+mapa = folium.Map(location=[-7.146954, -34.952295], zoom_start=16)
+
+# Adicionar uma camada de azulejos personalizada com opacidade ajustável
+folium.TileLayer(
+    tiles='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    attr='OpenStreetMap',
+    opacity=0.0,  # Ajuste a opacidade aqui
+).add_to(mapa)
+folium.LayerControl().add_to(mapa)
+
+# Adiciona o conteúdo de cada arquivo KML ao mapa
+for kmz_file in kmz_files:
+    kml_file = kmz_to_kml(kmz_file, output_dir)
+    if kml_file:
+        add_kml_to_map(kml_file, mapa, line_color='black')
 
 # Caminho para o marcador personalizado
 marcadores = {
-    'carcara': '/Users/matias/Documents/Programação/mapa_especies/carcara.png',
-    'quero': '/Users/matias/Documents/Programação/mapa_especies/quero.png',
-    'urubu': '/Users/matias/Documents/Programação/mapa_especies/urubu.png',
-    'garça': '/Users/matias/Documents/Programação/mapa_especies/garça.png',
-    'raposa': '/Users/matias/Documents/Programação/mapa_especies/raposa.png',
-    'tatu': '/Users/matias/Documents/Programação/mapa_especies/tatu.png'
+    'carcara': '/Users/matias/Documents/Programação/mapa_especies/png/carcara.png',
+    'quero': '/Users/matias/Documents/Programação/mapa_especies/png/quero.png',
+    'garça': '/Users/matias/Documents/Programação/mapa_especies/png/garça.png',
+    'urubu': '/Users/matias/Documents/Programação/mapa_especies/png/urubu.png',
+    'tatu': '/Users/matias/Documents/Programação/mapa_especies/png/tatu.png',
+    'raposa': '/Users/matias/Documents/Programação/mapa_especies/png/raposa.png'
 }
-
 
 # Adicionar marcadores para cada ponto geográfico na planilha
 for index, row in df.iterrows():
-    especie = row['Especie'].lower()  # Converter a espécie para minúsculas para evitar problemas de case-sensitive
-    
+    especie = row['Especie'].lower()
     if especie in marcadores:
-        # Criar um ícone personalizado para cada ponto
         icon = folium.CustomIcon(icon_image=marcadores[especie], icon_size=(20, 20))
-        
-        # Adicionar o marcador ao mapa
         folium.Marker([row['Lat'], row['Long']], popup=row['Quadrante'], icon=icon).add_to(mapa)
-
-        # Debugging: Verificar as coordenadas e a espécie
         print(f"Adicionando marcador: {row['Lat']}, {row['Long']} - Espécie: {row['Especie']}")
     else:
         print(f"Espécie não reconhecida: {row['Especie']}")
-
 
 # Salvar o mapa como um arquivo HTML
 mapa.save('/Users/matias/Documents/Programação/mapa_especies/mapa.html')
